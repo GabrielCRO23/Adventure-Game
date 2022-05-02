8cb808db2040c2df7834d0fa21a7306afce10e98
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-05-02 07:39:45 -0400

    --all

--- javascript.js
+++ javascript.js
@@ -1,5 +1,48 @@
 const gameBoard = (() => { 
+    
+    const startGame = document.querySelector('#startGame')
+    const options = document.querySelector('#options')
+    const gameBoard1 = document.querySelector('#gameBoard1')
+    const startingScreen = document.querySelector('#startingScreen')
+    
+    document.onkeydown = function (event) {
+        switch (event.keyCode) {
+           case 38:
+              startGame.focus();
+              break;
+           
+           case 40:
+              options.focus();
+              break;
+        }
+     };
 
+    
+
+        ['click','keypress'].forEach( evt => 
+            startGame.addEventListener(evt, function(){
+                gameBoard1.style.display = 'grid';
+                startingScreen.style.display = 'none';
+
+            })
+        );
+
+        
+
+var messageArray = ['YOU ARE ABOUT TO EMBARK ON A JOURNEY...']
+var textPosition = 0;
+var speed = 75;
+
+typeWriter = () => {
+    document.querySelector("#animate").
+    innerHTML = messageArray[0].substring(0, textPosition);
+    if (textPosition++ != messageArray[0].length)
+    setTimeout(typeWriter, speed)
+}
+
+window.addEventListener("load", typeWriter)
+    
+    
    
     const WINNING_COMBINATIONS = [
         [0, 1, 2],
@@ -21,7 +64,7 @@ const gameBoard = (() => {
     player1 = playerFactory('player 1', 'X', false, true)
     player2 = playerFactory('player 2', 'O', false, false)
 
-    const cellElements = document.querySelectorAll('[data-cell]')
+    const cellElements = document.querySelectorAll('.cell')
     
     
     function swapTurns() {
@@ -51,26 +94,22 @@ const gameBoard = (() => {
         //    })
       //  }
 
-        function endGameCheck() {
-            if (checkWins(currentClass) == true) {
-                alert();
-            }
-        }
+    
 
     cellElements.forEach ( cellElements => {
-        cellElements.addEventListener('click', function(){
-            if (player1.turn == true) {
+        cellElements.addEventListener('click', function(e){
+            if (player1.turn == true && player1.bot == false && !e.target.classList.contains(player1.symbol) && !e.target.classList.contains(player2.symbol)) {
                 currentClass = player1.symbol
                 cellElements.textContent = player1.symbol
-                cellElements.classList.add('X')
+                cellElements.classList.add(player1.symbol)
                 
                 
                 swapTurns()
                 endGameCheck()
-            } else if (player2.turn == true) {
+            } else if (player2.turn == true && player2.bot == false && !e.target.classList.contains(player1.symbol) && !e.target.classList.contains(player2.symbol)) {
                 currentClass = player2.symbol
                 cellElements.textContent = player2.symbol
-                cellElements.classList.add('O')
+                cellElements.classList.add(player2.symbol)
 
                 
                 swapTurns()
@@ -79,7 +118,11 @@ const gameBoard = (() => {
         })
     })
 
-
+    function endGameCheck() {
+        if (checkWins(currentClass) == true) {
+            alert();
+        }
+    }
 
 
 
